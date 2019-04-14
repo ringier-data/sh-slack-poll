2f8c4cdaf223de289d4476142c767204123d9f08
Zhao Wang <zhaow.km@gmail.com>
2019-04-14 21:22:31 +0200

    [OPS] further shortened the field length

--- main/migrations/0001_initial.py
+++ main/migrations/0001_initial.py
@@ -10,8 +10,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Teams',
             fields=[
-                ('access_token', models.CharField(max_length=1000)),
-                ('team_name', models.CharField(max_length=1000)),
+                ('access_token', models.CharField(max_length=255)),
+                ('team_name', models.CharField(max_length=255)),
                 ('team_id', models.CharField(max_length=255, primary_key=True, serialize=False)),
                 ('incoming_webhook_url', models.CharField(max_length=1000)),
                 ('incoming_webhook_configuration_url', models.CharField(max_length=1000)),
@@ -23,9 +23,9 @@ class Migration(migrations.Migration):
             name='Polls',
             fields=[
                 ('timestamp', models.CharField(max_length=100, primary_key=True, serialize=False, unique=True)),
-                ('channel', models.CharField(max_length=1000)),
+                ('channel', models.CharField(max_length=255)),
                 ('question', models.CharField(max_length=1000)),
-                ('options', models.CharField(max_length=1000)),
+                ('options', models.CharField(max_length=100)),
             ],
         ),
         migrations.CreateModel(
@@ -34,7 +34,7 @@ class Migration(migrations.Migration):
                 ('vote_id', models.AutoField(primary_key=True, serialize=False)),
                 ('poll', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='main.Polls')),
                 ('option', models.CharField(default='', max_length=100)),
-                ('users', models.CharField(default=[], max_length=1000)),
+                ('users', models.CharField(default=[], max_length=255)),
             ],
         ),
     ]
