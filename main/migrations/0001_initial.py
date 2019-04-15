f4fdb9b7d123dab2b77d9e25217f032ed4ace82b
Zhao Wang <zhaow.km@gmail.com>
2019-04-15 11:33:25 +0200

    [BUGFIX] database column width expanded

--- main/migrations/0001_initial.py
+++ main/migrations/0001_initial.py
@@ -13,8 +13,8 @@ class Migration(migrations.Migration):
                 ('access_token', models.CharField(max_length=255)),
                 ('team_name', models.CharField(max_length=255)),
                 ('team_id', models.CharField(max_length=255, primary_key=True, serialize=False)),
-                ('incoming_webhook_url', models.CharField(max_length=1000)),
-                ('incoming_webhook_configuration_url', models.CharField(max_length=1000)),
+                ('incoming_webhook_url', models.CharField(max_length=1024)),
+                ('incoming_webhook_configuration_url', models.CharField(max_length=1024)),
                 ('last_changed', models.DateTimeField(auto_now=True)),
                 ('created', models.DateTimeField(auto_now_add=True)),
             ],
@@ -25,7 +25,7 @@ class Migration(migrations.Migration):
                 ('timestamp', models.CharField(max_length=100, primary_key=True, serialize=False, unique=True)),
                 ('channel', models.CharField(max_length=255)),
                 ('question', models.CharField(max_length=1000)),
-                ('options', models.CharField(max_length=100)),
+                ('options', models.CharField(max_length=2000)),
             ],
         ),
         migrations.CreateModel(
@@ -34,7 +34,7 @@ class Migration(migrations.Migration):
                 ('vote_id', models.AutoField(primary_key=True, serialize=False)),
                 ('poll', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='main.Polls')),
                 ('option', models.CharField(default='', max_length=100)),
-                ('users', models.CharField(default=[], max_length=255)),
+                ('users', models.CharField(default=[], max_length=1000)),
             ],
         ),
     ]
--- main/models.py
+++ main/models.py
@@ -3,20 +3,20 @@ from django.db import models
 
 
 class Teams(models.Model):
-    access_token = models.CharField(max_length=1000)
-    team_name = models.CharField(max_length=1000)
+    access_token = models.CharField(max_length=255)
+    team_name = models.CharField(max_length=255)
     team_id = models.CharField(primary_key=True, max_length=255, serialize=False)
-    incoming_webhook_url = models.CharField(max_length=1000)
-    incoming_webhook_configuration_url = models.CharField(max_length=1000)
+    incoming_webhook_url = models.CharField(max_length=1024)
+    incoming_webhook_configuration_url = models.CharField(max_length=1024)
     last_changed = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
 
 class Polls(models.Model):
     timestamp = models.CharField(max_length=100, primary_key=True, serialize=False, unique=True)
-    channel = models.CharField(max_length=1000)
+    channel = models.CharField(max_length=255)
     question = models.CharField(max_length=1000)
-    options = models.CharField(max_length=1000)
+    options = models.CharField(max_length=2000)
 
 
 class Votes(models.Model):
